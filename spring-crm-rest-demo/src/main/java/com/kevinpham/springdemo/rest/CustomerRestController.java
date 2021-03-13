b12ea470a8a6ac201718b3e5382e633838ac4014
Kevin Pham <phambamkevin@gmail.com>
2021-03-13 12:34:36 -0800

    Add Customer

--- spring-crm-rest-demo/src/main/java/com/kevinpham/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/kevinpham/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,7 @@ public class CustomerRestController {
 	@Autowired
 	private CustomerService customerService;
 
+	
 	// mapping for GET /customers -> Get all customers
 	@GetMapping("/customers")
 	public List<Customer> getCustomers() {
@@ -27,6 +30,7 @@ public class CustomerRestController {
 		return customerService.getCustomers();
 	}
 
+	
 	// mapping for GET /customer/{customerId} -> Get a single customer by their ID
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomers(@PathVariable int customerId) {
@@ -40,6 +44,21 @@ public class CustomerRestController {
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		}
 
+		return theCustomer;
+	}
+	
+	
+	// mapping for POST /customers -> Add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		// Just in case user pass an ID in JSON ... set ID to 0
+		// This forces a save of new item ... instead of updating
+		theCustomer.setId(0);
+		
+		// Use @RequestBody to access the request body as a POJO and delegate task to CustomerService
+		customerService.saveCustomer(theCustomer);
+		
 		return theCustomer;
 	}
 
